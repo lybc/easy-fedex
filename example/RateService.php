c29fb717fc2c115f0c2b10480db9214b3b32e2e9
lybc <lybcli@163.com>
2017-02-24 16:28:17 +0800

    add example

--- example/RateService.php
+++ example/RateService.php
@@ -1,33 +1,38 @@
 <?php
+// 认证用户
 $auth = new \Fedex\Structures\Base\WebAuthenticationDetail();
 $auth->setUserCredential('mU2Zo7b1d7IxSYnh', '3vmHGKAWOYRzbyPbNkbYXZIV1');
 
 $client = new \Fedex\Structures\Base\ClientDetail();
 $client->setAccountNumber('510087240')->setMeterNumber('118780790');
 
+// wsdl 版本
 $version = new \Fedex\Structures\Base\Version();
 $version->setServiceId('crs')->setMajor('20')->setMinor('0')->setIntermediate('0');
 
-
+// 责任方信息
 $responsibleParty = (new \Fedex\Structures\ResponsibleParty())
     ->setAddress((new \Fedex\Structures\Base\Address())->setCountryCode('US'))
     ->setAccountNumber('xxx');
 
-
+// 付款人
 $payor = new \Fedex\Structures\Payor();
 $payor->setResponsibleParty($responsibleParty);
+
+// 付款相关信息
 $shippingChargesPayment = new \Fedex\Structures\ShippingChargePayment();
 $shippingChargesPayment->setPaymentType('SENDER');
 $shippingChargesPayment->setPayor($payor);
 
 //$requestPackageLineItems = new \Fedex\Structures\RequestedPackageLineItems();
 //$requestPackageLineItems->setDimensions()
+// 请求运输时需要的信息
 $requestShipment = new \Fedex\Structures\RequestedShipment();
 $requestShipment->setDropoffType('REGULAR_PICKUP')
-    ->setShipTimestamp()
-    ->setServiceType('INTERNATIONAL_PRIORITY')
-    ->setPackagingType('FEDEX_BOX')
-    ->setShipper(
+    ->setShipTimestamp()                                    // 运输发起时间
+    ->setServiceType('INTERNATIONAL_PRIORITY')              // 服务类型
+    ->setPackagingType('FEDEX_BOX')                         // 包裹类型
+    ->setShipper(                                           // 承运人
         (new \Fedex\Structures\Base\Contact())
             ->setPersonName('Sender Name')
             ->setCompanyName('Sender company name')
@@ -40,7 +45,7 @@ $requestShipment->setDropoffType('REGULAR_PICKUP')
             ->setCountryCode('US')
             ->setResidential(1)
     )
-    ->setRecipient(
+    ->setRecipient(                                         // 接收人
         (new \Fedex\Structures\Base\Contact())
             ->setPersonName('Recipient Name')
             ->setCompanyName('Recipient Company Name')
@@ -53,11 +58,11 @@ $requestShipment->setDropoffType('REGULAR_PICKUP')
             ->setCountryCode('US')
             ->setResidential(1)
     )
-    ->setShippingChargesPayment($shippingChargesPayment)
-    ->setPackageCount(1);
+    ->setShippingChargesPayment($shippingChargesPayment)    // 付款人
+    ->setPackageCount(1);                                   // 包裹数
 //    ->setRequestedPackageLineItems();
 
-$service = new \Fedex\Service\Rate();
+$service = new \Fedex\Service\Rate();                       // 服务调用
 $response = $service->set($auth)
     ->set($client)
     ->set($version)
