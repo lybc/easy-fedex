b631965b1b7f95f56bacba6208909f89fc7f2f30
lybc <lybcli@163.com>
2017-03-10 10:03:50 +0800

    gh-pages init

--- example/TrackService.php
+++
@@ -1,6 +0,0 @@
-<?php
-use Fedex\Service\TrackService;
-$service = new TrackService();
-$response = $service->setUserCredential('xx', 'xx', 'xx', 'xx')
-    ->setSelectionDetails('TRACKING_NUMBER_OR_DOORTAG', '808052658881')->call();
-var_dump($response);
